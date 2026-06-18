8daa835e596f2d71967d29e5c477b71bacd98ac6
Andrew Masiye <amasiye313@gmail.com>
2026-06-18 12:23:40 +0200

    Add Doria IntelliJ token types

---
+++ editors/intellij/doria/src/main/kotlin/dev/doria/intellij/highlighting/DoriaTokenTypes.kt
@@ -0,0 +1,31 @@
+package dev.doria.intellij.highlighting
+
+import com.intellij.psi.TokenType
+import com.intellij.psi.tree.IElementType
+import dev.doria.intellij.DoriaLanguage
+
+class DoriaTokenType(debugName: String) : IElementType(debugName, DoriaLanguage)
+
+object DoriaTokenTypes {
+    val WHITE_SPACE: IElementType = TokenType.WHITE_SPACE
+    val BAD_CHARACTER: IElementType = TokenType.BAD_CHARACTER
+
+    val KEYWORD = DoriaTokenType("DORIA_KEYWORD")
+    val MODIFIER = DoriaTokenType("DORIA_MODIFIER")
+    val PRIMITIVE_TYPE = DoriaTokenType("DORIA_PRIMITIVE_TYPE")
+    val COLLECTION_TYPE = DoriaTokenType("DORIA_COLLECTION_TYPE")
+    val TYPE_NAME = DoriaTokenType("DORIA_TYPE_NAME")
+    val IDENTIFIER = DoriaTokenType("DORIA_IDENTIFIER")
+    val VARIABLE = DoriaTokenType("DORIA_VARIABLE")
+    val THIS = DoriaTokenType("DORIA_THIS")
+    val BOOLEAN_LITERAL = DoriaTokenType("DORIA_BOOLEAN_LITERAL")
+    val NULL_LITERAL = DoriaTokenType("DORIA_NULL_LITERAL")
+    val NUMBER = DoriaTokenType("DORIA_NUMBER")
+    val STRING = DoriaTokenType("DORIA_STRING")
+    val COMMENT = DoriaTokenType("DORIA_COMMENT")
+    val OPERATOR = DoriaTokenType("DORIA_OPERATOR")
+    val BRACE = DoriaTokenType("DORIA_BRACE")
+    val BRACKET = DoriaTokenType("DORIA_BRACKET")
+    val PAREN = DoriaTokenType("DORIA_PAREN")
+    val PUNCTUATION = DoriaTokenType("DORIA_PUNCTUATION")
+}
